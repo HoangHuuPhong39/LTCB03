0d521f5e05da3e801cb11b9e0946182a43a47da9
HHPhong <phongluzy0902@gmail.com>
2025-03-02 22:20:27 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main (){
-    int t, n;
-    printf("nhap thang va nam: ");
-    scanf("%d%d", &t, &n);
-    for (t=1; t<=12, t++){
-        if (t=2){
-            if (n % 400 == 0){}
-            printf("thang %d nam %d co 28 ngay");
-        }else printf("thang %d nam %d co 29 ngay");
-        if ((t=4)||(t=6)){
-            if ((t=9)||(t=11)){
-                printf("thang %d nam %d co 30 ngay");
-            }
-            else printf("thang %d nam %d co 31 ngay");
-        }
+int main()
+{
+    float a, b, c, d, e, f, x, y;
+    printf("nhap vao cac he so thuc: ");
+    scanf("%f%f%f%f%f%f", &a, &b, &c, &d, &e, &f);
+    if ( d==0){
+    y = (d*c - f*a)/(d*b-a*e);
+    x = (c - b*y)/a;
     }
+    else {
+    y = (d*c - f*a)/(d*b-a*e);
+    x = (f - e*y)/d;
+    }
+    printf(" nghiem cua he phuong trinh la (%f; %f)", x, y);
     return 0;
 }
